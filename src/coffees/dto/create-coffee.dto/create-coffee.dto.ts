fc265e76fed2293cba8931f9ab624f9bcfaa5109
Mohamed-98 <m.gado86@gmail.com>
2022-12-31 00:26:55 +0200

    .

---
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -0,0 +1,8 @@
+export class CreateCoffeeDto {
+  readonly name: string;
+  readonly best_flaver: string[];
+  readonly age: number;
+  readonly address: string;
+  readonly phone: number;
+  readonly email: string;
+}
