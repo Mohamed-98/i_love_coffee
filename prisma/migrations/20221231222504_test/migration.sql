eada5313eaafdf94124c03001e0e578eebb94f7b
Mohamed-98 <m.gado86@gmail.com>
2023-01-01 00:54:37 +0200

    pirsma

---
+++ prisma/migrations/20221231222504_test/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `best_flaver` VARCHAR(191) NOT NULL,
+    `age` INTEGER NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `phone` INTEGER NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
